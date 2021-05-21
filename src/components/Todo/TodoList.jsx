2804e9914aa4fdd9957a0e2512995ed98000434d
svetoch-27 <82469770+svetoch-27@users.noreply.github.com>
2021-05-21 11:45:30 +0300

    Ошибка работы кнопок

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect, useCallback} from 'react';
 import Todos from './Todos';
 import axios from 'axios';
 import styles from '../../css/TodoCSS.module.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function TodoList() {
   const apiURL = 'http://185.246.66.84:3000/sarhipenkova/tasks';
@@ -9,8 +10,7 @@ function TodoList() {
   const [error, setError] = useState(''); 
   const apiURLSubTask = 'http://185.246.66.84:3000/sarhipenkova/subtasks';
 
-  // function addData() {
-  useEffect(() => {
+  function addData() {
     axios
     .get(apiURL)
     .then(async res => { 
@@ -28,6 +28,10 @@ function TodoList() {
       setList(myList)
     })
     .catch(err => setError(err))
+  }
+
+  useEffect(() => {  
+    addData();    
   },[] 
   )  
 
@@ -43,10 +47,13 @@ function TodoList() {
   const onDeleteTask = useCallback((id) => {
     axios.delete(apiURL+'/'+id)  
       .then(response => {
-        setList(prev => prev.filter(curr => curr.id !== id));
+        debugger
+        // setList(prev => prev.filter(curr => curr.id !== id));
+        addData();   
+        
       })
       .catch(err => setError(err));
-    },[setList])
+    },[])
 
   const onRestoreTask = useCallback((task) => {
     const newTask = {
@@ -57,7 +64,7 @@ function TodoList() {
 
     axios.put(apiURL+'/'+task.id,newTask)  
       .then(response => {
-        setList(prev => { return [...prev.filter(curr => curr.id !== task.id), response.data]});
+        // setList(prev => { return [...prev.filter(curr => curr.id !== task.id), response.data]});
       })
       .catch(err => setError(err));
     },[setList])
@@ -71,7 +78,7 @@ function TodoList() {
 
     axios.put(apiURL+'/'+task.id,newTask)  
       .then(response => {
-        setList(prev => { return [...prev.filter(curr => curr.id !== task.id), response.data]});
+        // setList(prev => { return [...prev.filter(curr => curr.id !== task.id), response.data]});
       })
       .catch(err => setError(err));
     },[setList])
@@ -86,7 +93,7 @@ function TodoList() {
     
     axios.post(apiURL,newTask)  
       .then(response => {
-        setList(prev => [...prev, response.data]);
+        // setList(prev => [...prev, response.data]);
       })
       .catch(err => setError(err));},[setList])
 
@@ -100,49 +107,144 @@ function TodoList() {
     axios.put(apiURL+'/'+task.id,newTask)  
     
       .then(response => {
-        setList(prev => {return [...prev.filter(curr => curr.id !== task.id), response.data]});
+        // setList(prev => {return [...prev.filter(curr => curr.id !== task.id), response.data]});
       })
       
       .catch(err => setError(err));
     },[])
+
+    // function handleOnDradEnd(result)
+    // {console.log(result)}
+
+    // ----------------------------------------------- start    dnd   --------------------------------   
+    
+  // const [currentTask,setCurrentTask] = useState(null);   
+
+  // const dragStartHandler= (e, task) => {
+  //     // e.preventDefault();
+  //     console.log('handleOnEdit   id = ' + task.id+'  seq = '+task.sequence);
+  //     setCurrentTask(task)
+  //   }
+
+  // const dragLeaveHandler= (e) => {
+  //     // e.preventDefault();111111111
+  //     console.log('dragLeaveHandler+++++++++++++++++++++!!!');
+  //   }
+
+  // const dragEndHandler= (e) => {
+  //     // e.preventDefault();      
+  //     e.target.style.background = 'while';
+  //     console.log('dragEndHandler------------------------   END');
+  //   }
+
+  // const dragOverHandler= (e) => {
+  //     e.preventDefault();
+  //     e.target.style.background = 'lightgray';
+  //     console.log('dragOverHandler ----------------------   ---');
+  //   }
+
+  // const dropHandler= (e, task) => {
+  //     e.preventDefault();
+  //     setList(list.map(item => {
+  //       if (item.id === task.id){
+  //       return {...item, sequence: currentTask.sequence}
+  //       }
+  //       if (item.id === currentTask.id){
+  //         return {...item, sequence: task.sequence}
+  //       }
+  //       return item
+  //     }
+  //     ))          
+  //     e.target.style.background = 'while';
+  //     console.log('-----------   dropHandler    id = ' + task.id+'  seq = '+task.sequence);
+  //   }
+
+  // const sortTasks = (a, b) => {
+  //     if (a.sequence > b.sequence){
+  //       return 1
+  //     }else{
+  //       return -1
+  //     }
+  //   }
+
+    // ------------------------ end dnd
   
-  return (    
+  return (   
+    
     <> 
       <h1>Тестовое задание</h1>
       <h2>Активные задачи</h2>
       <div className={styles.center}>
         <button className={styles.classTodo__button} onClick={addTodo}>Добавить задачу</button>
       </div>
-      <div className={styles.tasks, styles.task__activ}>  
-        {tasksActiv.map((task) => (  
-          <Todos
-            task={task}
-            onChangeCheck={onChangeCheck}            
-            onDeleteTask={onDeleteTask}
-            onHandleEdit={onHandleEdit}
-            onRestoreTask={onRestoreTask}
-          />
-          )
-        )}
+      <div className={styles.tasks, styles.task__activ} > 
+
+      {/* <DragDropContext> 
+        <Droppable> */}
+       {/* onDragEnd={handleOnDradEnd}> */}
+        {/* {(provided) =>  */}
+              
+        {/* {tasksActiv.sort(sortTasks).map((task) => (   */}
+              {tasksActiv.map((task) => (  
+                <div
+                    // draggable={true}                  
+                    // onDragStart={(e) => dragStartHandler(e, task)}
+                    // onDragLeave={(e) => dragLeaveHandler(e)}
+                    // onDragEnd={(e) => dragEndHandler(e)}
+                    // onDragOver={(e) => dragOverHandler(e)}
+                    // onDrop={(e) => dropHandler(e, task)}                 
+                >
+                <Todos                  
+                  task={task}                  
+                  // draggable={true}                  
+                  //   onDragStart={(e) => dragStartHandler(e, task)}
+                  //   onDragLeave={(e) => dragLeaveHandler(e)}
+                  //   onDragEnd={(e) => dragEndHandler(e)}
+                  //   onDragOver={(e) => dragOverHandler(e)}
+                  //   onDrop={(e) => dropHandler(e, task.sequence)}
+                  onChangeCheck={onChangeCheck}            
+                  onDeleteTask={onDeleteTask}
+                  onHandleEdit={onHandleEdit}
+                  onRestoreTask={onRestoreTask}
+                />
+                </div>
+                )
+              )}
+         {/* } */}
+        {/* </Droppable>
+      </DragDropContext>  */}
       </div>
 
       <div className={styles.indent}></div>
       
       <h2>Завершенные задачи</h2>
       {/* <div className={styles.tasks, styles.task__completed}> */}
-      <div className={styles.task__completed}>
+      <div 
+          className={styles.task__completed}        
+          draggable={true}>
         
-        {tasksComplited.map((task)=> (
-          <Todos
-            task={task}
-            onRestoreTask={onRestoreTask}
-          />)
-        )}
+          {tasksComplited.map((task)=> (
+            <div  
+                className={styles.task__completed}        
+                draggable={true}
+                    // onDragStart={(e) => dragStartHandler(e, task.sequence)}
+                    // onDragLeave={(e) => dragLeaveHandler(e)}
+                    // onDragEnd={(e) => dragEndHandler(e)}
+                    // onDragOver={(e) => dragOverHandler(e)}
+                    // onDrop={(e) => dropHandler(e, task.sequence)}
+                    >
+            <Todos
+              task={task}
+              onRestoreTask={onRestoreTask}
+            />
+            </div>)
+          )}
       </div>   
       <div className={styles.footer}></div>   
     </>
+    
   );
-        }
+          }
 
 
 // TodoList.propTypes = {
--- src/components/Todo/Todos.jsx
+++ src/components/Todo/Todos.jsx
@@ -2,11 +2,14 @@ import styles from '../../css/TodoCSS.module.css';
 // import Subtodos from './Subtodos';
 import { useState} from 'react';
 import PropTypes from 'prop-types';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
   
 function Todos({task, onHandleEdit, onDeleteTask, onRestoreTask, onChangeCheck, onChangeTaskName}) {
     
   const [editValue, setEditValue] = useState(task.title);
-  const [onEdit, setOnEdit] = useState(false);       
+  const [onEdit, setOnEdit] = useState(false);      
+  let subTasksLength = 0; 
         
   const onHandleSave =(task) =>{
     setOnEdit(false)
@@ -18,10 +21,16 @@ function Todos({task, onHandleEdit, onDeleteTask, onRestoreTask, onChangeCheck, 
         }
     }
 
-    const handleOnEdit = () => {
-      setOnEdit(true)
+    const handleOnEdit = (e) => {
+      setOnEdit(true);
+      e.preventDefault();
+      console.log('handleOnEdit');
     }
+
+
+    console.log('  Array.isArray(task.subTasks)  ===========  '+Array.isArray(task.subTasks));
     
+
     if (onEdit){ 
       return (
         <div className={styles.classTodo}>
@@ -30,51 +39,90 @@ function Todos({task, onHandleEdit, onDeleteTask, onRestoreTask, onChangeCheck, 
               <input className={styles.classTodo__checkbox} type='checkbox' disabled={!task.completed} defaultChecked={!task.completed} onChange={() => onChangeCheck(task)}></input>
               <input className={styles.classTodo__taskname} type="text" placeholder={editValue} value = {editValue} onChange={e => setEditValue(e.target.value)}/> 
               
-              </div>
+            </div>
             <div className={styles.out_button}>        
               {task.completed && <button className={styles.classTodo__button} onClick={() => {onHandleSave(task)}} >Сохранить</button>}      
               {task.completed && <button className={styles.classTodo__button} onClick={() => onDeleteTask(task.id)}>Удалить</button>}               
               {!task.completed && <button className={styles.classTodo__button} onClick={() => onRestoreTask(task)}>Восстановить</button>}
               </div>
           </div>     
-          <div className={styles.out_subtasks}>    
-            {task.subTasks.map(item => console.log(item.title))}
-            {task.subTasks.map(item => 
-              <div className={styles.subtask}><p>{item.title}</p></div>)}
-            </div>
+          {/* {if (Array.isArray(task.subTasks)){
+          {subTasksLength = task.subTasks.length;} --------------------------- !!!*/}
+          {Array.isArray(task.subTasks) && (
+            <div className={styles.out_subtasks}>    
+              {task.subTasks.map(item => console.log(item.title))}
+              {task.subTasks.map(item => 
+                <div className={styles.subtask}><p>{item.title}</p></div>)}
+            </div>)}
                         
           </div>
            )
       }else{
-      return (        
-        // <div className={styles.classTodo, styles.style_column}>
-        <div className={styles.classTodo}>
-          <div className={styles.style_row}>
-            <div className={styles.out_input}>
-              <input className={styles.classTodo__checkbox} type='checkbox' disabled={!task.completed} defaultChecked={!task.completed} onChange={() => onChangeCheck(task)}></input>
-              <input className={styles.classTodo__taskname} type="text" placeholder={task.title} id={task.id} value = {editValue} onChange={onChangeTaskName}/>                  
+        if (Array.isArray(task.subTasks)){
+          subTasksLength = task.subTasks.length;
+        }
+        if (subTasksLength !== 0 ) {
+          return (        
+            // <div className={styles.classTodo, styles.style_column}>
+            <div className={styles.classTodo}>
+              <div className={styles.style_row}>
+                <div className={styles.out_input}>
+                  <input className={styles.classTodo__checkbox} type='checkbox' disabled={!task.completed} defaultChecked={!task.completed} onChange={() => onChangeCheck(task)}></input>
+                  <input className={styles.classTodo__taskname} type="text" placeholder={task.title} id={task.id} value = {editValue} onChange={onChangeTaskName}/>                  
+                </div>
+                <div className={styles.out_button}> 
+                  {task.completed && <button className={styles.classTodo__button} onClick={handleOnEdit} >Редактировать</button>}                    
+                  {task.completed && <button className={styles.classTodo__button} onClick={() => onDeleteTask(task.id)}>Удалить</button>}               
+                  {!task.completed && <button className={styles.classTodo__button} onClick={() => onRestoreTask(task)}>Восстановить</button>}
+                </div>
+              </div>     
+              <div className={styles.out_subtasks}>    
+                {/* {task.subTasks.map(item => console.log(item.title))} */}
+                {task.subTasks.map(item => 
+                  <div className={styles.subtask}>
+                    <p>{item.title}</p>
+                  </div>)}
+                </div>          
             </div>
-            <div className={styles.out_button}> 
-              {task.completed && <button className={styles.classTodo__button} onClick={handleOnEdit} >Редактировать</button>}                    
-              {task.completed && <button className={styles.classTodo__button} onClick={() => onDeleteTask(task.id)}>Удалить</button>}               
-              {!task.completed && <button className={styles.classTodo__button} onClick={() => onRestoreTask(task)}>Восстановить</button>}
-            </div>
-          </div>     
-          <div className={styles.out_subtasks}>    
-            {/* {task.subTasks.map(item => console.log(item.title))} */}
-            {task.subTasks.map(item => 
-              <div className={styles.subtask}>
-                <p>{item.title}</p>
-              </div>)}
-            </div>
-          
-        </div>
-        )
+            )
+            }else{
+              return (        
+                // <div className={styles.classTodo, styles.style_column}>
+                <div 
+                    className={styles.classTodo} 
+                    // draggable={true}
+                    // onDragStart={(e) => dragStartHandler(e, task.sequence)}
+                    // onDragLeave={(e) => dragLeaveHandler(e)}
+                    // onDragEnd={(e) => dragEndHandler(e)}
+                    // onDragOver={(e) => dragOverHandler(e)}
+                    // onDrop={(e) => dropHandler(e, task.sequence)}
+                    >
+                  <div className={styles.style_row}>
+                    <div className={styles.out_input}>
+                      <input className={styles.classTodo__checkbox} type='checkbox' disabled={!task.completed} defaultChecked={!task.completed} onChange={() => onChangeCheck(task)}></input>
+                      <input className={styles.classTodo__taskname} type="text" placeholder={task.title} id={task.id} value = {editValue} onChange={onChangeTaskName}/>                  
+                    </div>
+                    <div className={styles.out_button}> 
+                      {task.completed && <button className={styles.classTodo__button} onClick={handleOnEdit} >Редактировать</button>}                    
+                      {task.completed && <button className={styles.classTodo__button} onClick={() => onDeleteTask(task.id)}>Удалить</button>}               
+                      {!task.completed && <button className={styles.classTodo__button} onClick={() => onRestoreTask(task)}>Восстановить</button>}
+                    </div>
+                  </div>    
+                </div>
+                )
+            }
+        
       }
   }
 
   Todos.propTypes = {
-    task: PropTypes.arrayOf(PropTypes.object),
+    // task: PropTypes.arrayOf(PropTypes.object),
+    task: PropTypes.arrayOf({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      completed: PropTypes.bool,
+      sequence: PropTypes.number,
+    }),
   onHandleEdit: PropTypes.func,
   onDeleteTask: PropTypes.func,
   onRestoreTask: PropTypes.func,
